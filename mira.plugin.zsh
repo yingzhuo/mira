85259cfd4d31fee83668c5d36304124deb79c4c3
yingzhuo <yingzhor@gmail.com>
2020-11-25 12:13:15 +0800

    2020-11-25 11:13:51

--- mira.plugin.zsh
+++ mira.plugin.zsh
@@ -2,7 +2,7 @@
 # Description
 # -----------
 #
-# 添加一些常用的功能
+# 添加一些常用的功能和命令别名
 #
 # ------------------------------------------------------------------------------
 # Authors
@@ -10,6 +10,70 @@
 #
 # * 应卓 <github.com/yingzhuo>
 #
+# ------------------------------------------------------------------------------
+
+alias type='type -a'
+
+alias c='clear'
+alias cls='clear'
+
+alias vi='vim'
+alias edit='vim'
+alias emacs='vim'
+alias nano='vim'
+
+alias date='/bin/date "+%F %T"'
+alias timestamp='/bin/date +%s'
+
+alias more='less'
+
+alias ping='ping -c 5'
+
+alias shutdown='false'
+alias halt='false'
+alias reboot='false'
+alias poweroff='false'
+
+alias mkdir='mkdir -p'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+alias q='exit'
+
+if [[ $EUID -eq -0 ]]; then
+    alias root='true'
+else
+    alias root='su -'
+fi
+
+# ------------------------------------------------------------------------------
+
+# Editor
+export EDITOR='vim'
+
+# Time Style
+export TIME_STYLE=long-iso
+
+# Locale
+export LC_ALL=en_US.UTF-8
+
+# GPG
+export GPG_TTY=$(tty)
+
+# History
+export HISTSIZE=1500
+export HISTFILE=~/.zsh_history
+export HISTFILESIZE=1000
+export HISTCONTROL=ignoreboth
+
+# bin / sbin
+[[ -d $HOME/bin ]] && export PATH=$PATH:$HOME/bin
+[[ -d $HOME/sbin ]] && export PATH=$PATH:$HOME/sbin
+
+# 其他
+export PATH=.:$PATH
+
 # ------------------------------------------------------------------------------
 
 function __os() {
@@ -51,18 +115,25 @@ function __update_ntp() {
 
 case $(__os) {
   "CentOS")
+    # ---
     alias sys.reboot=__reboot
     alias sys.shutdown=__shutdown
     alias sys.update=__update_centos
     alias sys.clean=__clean_centos
     alias sys.ntp=__update_ntp
+    # ---
+    alias yum='sudo yum'
+    alias dnf='sudo dnf'
     ;;
   "Ubuntu")
     alias sys.reboot=__reboot
-    alais sys.shutdown=__shutdown
+    alias sys.shutdown=__shutdown
     alias sys.update=__update_ubuntu
     alias sys.clean=__clean_ubuntu
     alias sys.ntp=__update_ntp
+    # ---
+    alias apt-get='sudo apt-get'
+    alias apt='sudo apt'
     ;;
   *)
   ;;
